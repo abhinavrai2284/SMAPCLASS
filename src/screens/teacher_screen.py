15e8c13801417b7e1ab7a37b314b7bf5f1cabbfe
Abhinav Rai <abhinav63074347@gmail.com>
2026-08-14 15:26:25 +0530

    SMAPCLASS

--- src/screens/teacher_screen.py
+++ src/screens/teacher_screen.py
@@ -1,4 +1,35 @@
 import streamlit as st
 
+from src.ui.base_layout import style_background_dashboard, style_base_layout
+from src.screens.components.header import header_dashboard
+
 def teacher_screen():
-    st.header('Teacher Screen')
+
+    style_background_dashboard()
+    style_base_layout()
+
+    teacher_screen_login()
+
+
+   
+
+def teacher_screen_login():
+    c1, c2 = st.columns(2, vertical_alignment='center', gap='xxlarge')
+    with c1:
+        header_dashboard()
+    with c2:
+        if st.button("Go back to Home", type='secondary', key='loginbackbtn', shortcut="control+backspace"):
+           st.header("Register your teacher profile")
+
+    st.header('Login using password', text_alignment='center')
+
+def teacher_screen_register():
+    c1, c2 = st.columns(2, vertical_alignment='center', gap='xxlarge')
+    with c1:
+        header_dashboard()
+    with c2:
+        if st.button("Go back to Home", type='secondary', key='loginbackbtn', shortcut="control+backspace"):
+            
+
+           st.header('Register your teacher profile')
+        
